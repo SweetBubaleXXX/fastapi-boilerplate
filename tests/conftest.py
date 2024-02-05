5dd44b21d0495db338aab718b80ef85a173f5def
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2024-02-05 13:58:02 +0300

    Refactor conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,16 @@
-from typing import AsyncIterator
+from typing import AsyncIterator, Callable
 
 import pytest
 import pytest_asyncio
 from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 from sqlalchemy.pool import StaticPool
 
 from src.core.config import settings
@@ -13,17 +18,45 @@ from src.core.database import get_session
 from src.main import create_app
 from src.models.base import Base
 
-engine = create_async_engine(settings.TEST_DB_URL, poolclass=StaticPool)
-async_session_factory = async_sessionmaker(engine)
+
+@pytest.fixture(scope="session")
+def db_engine():
+    return create_async_engine(settings.TEST_DB_URL, poolclass=StaticPool)
 
 
-async def override_get_session() -> AsyncIterator[AsyncSession]:
-    async with async_session_factory() as session:
+@pytest.fixture(scope="session")
+def db_session_factory(db_engine: AsyncEngine):
+    return async_sessionmaker(db_engine)
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def setup_database(db_engine: AsyncEngine):
+    async with db_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+    async with db_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="session")
+def override_get_session(db_session_factory):
+    async def get_session() -> AsyncIterator[AsyncSession]:
+        async with db_session_factory() as session:
+            yield session
+
+    return get_session
+
+
+@pytest_asyncio.fixture
+async def db_session(override_get_session: Callable[[], AsyncIterator[AsyncSession]]):
+    async with await anext(override_get_session()) as session:
         yield session
 
 
 @pytest.fixture(scope="session")
-def app():
+def app(override_get_session: Callable[[], AsyncIterator[AsyncSession]]):
     app = create_app()
     app.dependency_overrides[get_session] = override_get_session
     return app
@@ -35,24 +68,7 @@ async def trigger_lifespan_events(app: FastAPI):
         yield
 
 
-@pytest_asyncio.fixture
-async def database():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-    yield override_get_session
-
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-
-
-@pytest_asyncio.fixture
-async def db_session(database):
-    async with database() as session:
-        yield session
-
-
 @pytest.fixture
-def client(app: FastAPI, database):
+def client(app: FastAPI):
     with TestClient(app) as client:
         yield client
